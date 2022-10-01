d9b0b6dbaa5bc398d086901d4f40ee21e5b625b2
selin <selinguneylg@gmail.com>
2022-10-02 00:13:18 +0300

    added project files

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var returnedCar in carManager.GetAll())
+            {
+                Console.WriteLine(returnedCar.Description);
+            }
+
+            Car car = new Car();
+            car.Id = 10;
+            car.BrandId = 11;
+            car.ColorId = 3;
+            car.ModelYear = "2015";
+            car.Description = "Peugeot 5008, Dizel, Otomatik";
+
+            carManager.Add(car);
+            Console.WriteLine("Yeni bir araba eklendikten sonra--------------------------"); 
+            foreach (var returnedCar in carManager.GetAll())
+            {
+                Console.WriteLine(returnedCar.Description);
+            }
+
+            Console.WriteLine("Bir araba silindikten sonra--------------------------"); 
+            carManager.Delete(car);
+
+            foreach (var returnedCar in carManager.GetAll())
+            {
+                Console.WriteLine(returnedCar.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal ()
+        {
+            _cars = new List<Car>
+            {
+                new Car{ Id=1, BrandId=1, ColorId=2, DailyPrice=545.92m, ModelYear="2010", Description="Renault Clio, Dizel/Benzin, Manuel"},
+                new Car{ Id=2, BrandId=3, ColorId=5, DailyPrice=621.63m, ModelYear="2018", Description="Fiat Egea, Dizel/Benzin, Manuel"},
+                new Car{ Id=3, BrandId=1, ColorId=7, DailyPrice=450.77m, ModelYear="2015", Description="Renaul Clio AT, Dizel/Benzin, Otomatik"},
+                new Car{ Id=4, BrandId=4, ColorId=6, DailyPrice=745.13m, ModelYear="2019", Description="Ford Kuga, Dizel/Benzin, Otomatik"},
+                new Car{ Id=5, BrandId=2, ColorId=10, DailyPrice=985.63m, ModelYear="2000", Description="BMW 2 Serisi, Dizel/Benzin, Otomatik"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = null;
+
+            carToDelete = _cars.SingleOrDefault(c=>c.Id == car.Id);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.Id == carId).ToList();
+
+            //where koşulu içindeki şartları uyan bütün elemanları yeni bir listeye dönüştürüp, onu döndürür.
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
